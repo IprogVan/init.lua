a55e0d7fc6bf45517eb0a42d95a8073169246b2b
Ivan Ebra <38085477+IprogVan@users.noreply.github.com>
2025-01-27 16:26:26 +0100

    Create treesitter.lua

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "java" },
+        highlight = { enable = true },
+      })
+    end,
+  }
+}
